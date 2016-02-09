45908c851198d06918234e7246f4e299ea66aaef
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-02-09 13:21:06 +0100

    cleanning

--- js/lib/meshes.js/viewcontrols.js
+++ js/lib/meshes.js/viewcontrols.js
@@ -43,7 +43,6 @@ var MeshesJS = MeshesJS || {};
     };
 
     ViewControls.prototype.setFocusAt = function(point) {
-        console.log(this.focusPoint.position)
         _.assign(this.focusPoint.position, point || {});
     };
 
