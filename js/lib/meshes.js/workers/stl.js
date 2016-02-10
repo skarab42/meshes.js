a2be1097ee2542085987d829895ca00eb6a08d1c
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-02-10 11:14:06 +0100

    STL loader

--- js/lib/meshes.js/workers/stl.js
+++ js/lib/meshes.js/workers/stl.js
@@ -17,44 +17,107 @@ function loadSTL(buffer) {
     var binary = reader.byteLength == (80 + 4 + 50 * faces);
 
     // geometry variables
-    var vertices, normals;
+    var vertices, normals, normalX, normalY, normalZ;
 
-    // parse
+    // parse binary
     if (binary) {
         var dataOffset = 84;
         var faceLength = 12 * 4 + 2;
         var bufferLength = faces * 3 * 3;
 
-        var vertices = new Float32Array(bufferLength);
-        var normals = new Float32Array(bufferLength);
+        vertices = new Float32Array(bufferLength);
+        normals = new Float32Array(bufferLength);
 
         var offset = 0;
+        var face, start, vertexstart;
 
-        for (var face = 0; face < faces; face ++) {
-            var start = dataOffset + face * faceLength;
-            var normalX = reader.getFloat32(start, true);
-            var normalY = reader.getFloat32(start + 4, true);
-            var normalZ = reader.getFloat32(start + 8, true);
+        for (face = 0; face < faces; face ++) {
+            start = dataOffset + face * faceLength;
+            normalX = reader.getFloat32(start, true);
+            normalY = reader.getFloat32(start + 4, true);
+            normalZ = reader.getFloat32(start + 8, true);
 
             for ( var i = 1; i <= 3; i ++ ) {
-                var vertexstart = start + i * 12;
-
-                vertices[offset] = reader.getFloat32(vertexstart, true);
-                vertices[offset + 1] = reader.getFloat32(vertexstart + 4, true);
-                vertices[offset + 2] = reader.getFloat32(vertexstart + 8, true);
+                vertexstart = start + i * 12;
 
                 normals[offset] = normalX;
                 normals[offset + 1] = normalY;
                 normals[offset + 2] = normalZ;
 
+                vertices[offset] = reader.getFloat32(vertexstart, true);
+                vertices[offset + 1] = reader.getFloat32(vertexstart + 4, true);
+                vertices[offset + 2] = reader.getFloat32(vertexstart + 8, true);
+
                 offset += 3;
             }
         }
-
-        postMessage({
-            normals: normals.buffer,
-            vertices: vertices.buffer
-        }, [ vertices.buffer, normals.buffer ]);
     }
 
+    // parse ASCII
+    else {
+        vertices = [];
+        normals = [];
+
+        var code, args;
+        var line = '';
+
+        var startTime = Date.now();
+
+        for (var i = 0; i < reader.byteLength; i++) {
+            // concact chars until new line
+            code = reader.getUint8(i);
+            if (code !== 10) {
+                line += String.fromCharCode(code);
+                continue;
+            }
+
+            // reduce spaces/tabs and trim witespaces
+            line = line.replace(/\s\s+/g, ' ').trim();
+
+            // skip line shorter than 12 chars (vertex 0 0 0)
+            if (line.length < 12) {
+                line = '';
+                continue;
+            }
+
+            // split line on spaces
+            args = line.split(' ');
+
+            // extract faces
+            if (args[0] === 'facet') {
+                normalX = parseFloat(args[2]);
+                normalY = parseFloat(args[3]);
+                normalZ = parseFloat(args[4]);
+            }
+            else if (args[0] === 'vertex') {
+                normals.push(normalX);
+                normals.push(normalY);
+                normals.push(normalZ);
+
+                vertices.push(parseFloat(args[1]));
+                vertices.push(parseFloat(args[2]));
+                vertices.push(parseFloat(args[3]));
+            }
+
+            // reset line
+            line = '';
+        }
+
+        var suffix = 'ms';
+        var time = Date.now() - startTime;
+        if (time > 1000) {
+            suffix = 's';
+            time /= 1000;
+        }
+        console.log('-> ' + time + suffix);
+
+        normals = new Float32Array(normals);
+        vertices = new Float32Array(vertices);
+    }
+
+    // send buffers
+    postMessage({
+        normals: normals.buffer,
+        vertices: vertices.buffer
+    }, [ vertices.buffer, normals.buffer ]);
 }
